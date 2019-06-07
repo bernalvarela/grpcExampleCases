625f7ab3f311a232855dd257c3004432bb147e97
bernal <bernalvarela@gmail.com>
2019-06-07 16:39:51 +0200

    -fixed compilation

--- grpcSpringBoot/src/main/java/com/corunet/grpc/GrpcApplication.java
+++ grpcSpringBoot/src/main/java/com/corunet/grpc/GrpcApplication.java
@@ -2,8 +2,10 @@ package com.corunet.grpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class GrpcApplication {
 
 	public static void main(String[] args) {
--- grpcSpringBoot/src/main/java/com/corunet/grpc/controller/GreeterController.java
+++ grpcSpringBoot/src/main/java/com/corunet/grpc/controller/GreeterController.java
@@ -1,19 +1,21 @@
 package com.corunet.grpc.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.corunet.service.Service.HelloReply;
 import com.corunet.service.Service.HelloRequest;
 
 import io.micrometer.core.annotation.Timed;
 
-@Controller
+@RestController
 @Timed("greeter")
 public class GreeterController {
 
-	@PostMapping("/greeter/sayHello")
+    @PostMapping(path= "/greeter/sayHello", consumes = "application/json", produces = "application/json")
 	public HelloReply sayHello(@RequestBody HelloRequest request) {
 		System.out.println("Name in received message: " + request.getName());
 		HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
